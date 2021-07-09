9e4f897243c7edf75ee4874a1fa9fb8c69f3ba34
R.S.K.Abbas <rskabbas@outlook.com>
2021-07-09 20:02:56 +0530

    Add files via upload

---
+++ hacekrranksolutions/stringsubstring.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static String getSmallestAndLargest(String s, int k) {
+        
+        //taking a default string for further replacement
+        String sus = s.substring(0, k);
+        String smallest = sus;
+        String largest = sus;
+        int a = s.length();
+        
+        for(int i = 1; i <= (a-k); i++){
+            sus = s.substring(i, (i + k));
+
+            if(sus.compareTo(smallest) < 0){   //comaparing the previous smallest to the current substring taken and replacing the smallest value to the variable
+                smallest = sus;
+            }
+            if(sus.compareTo(largest) > 0){ //comaparing the previous largest to the current substring taken and replacing the largest value to the variable
+                largest = sus;
+            }            
+        }
+        
+        
+        return smallest + "\n" + largest;
+    }
