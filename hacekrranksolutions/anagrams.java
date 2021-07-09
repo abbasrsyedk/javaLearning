77a209a773233568b8021cca3b187c6e29266381
R.S.K.Abbas <rskabbas@outlook.com>
2021-07-09 20:03:46 +0530

    Add files via upload

---
+++ hacekrranksolutions/anagrams.java
@@ -0,0 +1,37 @@
+static boolean isAnagram(String a, String b) {
+        // Complete the function
+        String temp = "";
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+        if(a.length()==b.length()){
+            for(int i=0; i<a.length();i++){
+                for(int j=0; j<b.length(); j++){
+                    if((a.charAt(i))==(b.charAt(j))){
+                        temp += b.charAt(j);
+                        break;
+                    }   
+                }
+            }
+            //System.out.println(temp);
+            if(temp.equals(a)){
+                return true;
+        }
+            else{
+                return false;
+        }
+        }
+        else{
+            return false;
+        }
+        
+    }
+    public static void main(String[] args) {
+    
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
