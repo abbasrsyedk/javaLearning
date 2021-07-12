64bb519e918ec85f194a87fb54533fe95c4f5b5f
R.S.K.Abbas <rskabbas@outlook.com>
2021-07-12 22:39:56 +0530

    Add files via upload

---
+++ topics in java/INHERITOOP.java
@@ -0,0 +1,14 @@
+class Calculator {
+
+  public int add(int i, int j) {
+    return i + j;
+  }
+}
+
+public class INHERITOOP {
+
+  public static void main(String[] args) {
+    Calculator c1 = new Calculator();
+    int result = c1.add(5, 4);
+  }
+}
