dc3c7597cb0d3fa9145729c38904f546035d449a
R.S.K.Abbas <rskabbas@outlook.com>
2021-08-04 23:02:13 +0530

    Add files via upload

---
+++ topics in java/classes.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Person {
+    String name;
+    int age;
+}
+
+public class classes {
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        person1.name = "Abbas";
+        person1.age = 20;
+
+        Person person2 = new Person();
+        person2.name = "Divija";
+        person2.age = 20;
+
+    }
+}
